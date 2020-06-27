32322c5adfaca65c469f4c215466f43d156fe30a
Andre Scherrer <device2code@gmail.com>
2020-06-27 19:41:28 -0300

    TDD: Email & Date Fields

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Contact;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -35,6 +36,27 @@ class ContactsTest extends TestCase
                 $response->assertSessionHasErrors($field);
                 $this->assertCount(0, Contact::all());
             });
+    }
+
+    /** @test */
+    public function email_must_be_a_valid_email()
+    {
+        $response = $this->post('/api/contacts', 
+            array_merge($this->data(), ['email' => 'NOT AN EMAIL']));
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /** @test */
+    public function birthdays_are_properly_stored()
+    {
+        $response = $this->post('/api/contacts', 
+            array_merge($this->data()));
+        
+        $this->assertCount(1, Contact::all());
+        $this->assertInstanceOf(Carbon::class, Contact::first()->birthday);
+        $this->assertEquals('06-11-1980', Contact::first()->birthday->format('m-d-Y'));
     }
 
     private function data()
